125c06a80c0a9825f38b72a27d3ffb410337892e
Enbani <enbani.moore@gmail.com>
2021-03-02 21:26:07 -0500

    adding init wallet

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const INITIAL_DIFFICULTY = 3;
 const MINE_RATE = 1000; //mine rate in milliseconds
+const STARTING_BALANCE = 1000;
 
 const GENESIS_DATA = {
   timestamp: 1,
@@ -12,4 +13,9 @@ const GENESIS_DATA = {
 };
 
 
-module.exports = { GENESIS_DATA, INITIAL_DIFFICULTY, MINE_RATE };
+module.exports = {
+  GENESIS_DATA,
+  INITIAL_DIFFICULTY,
+  MINE_RATE,
+  STARTING_BALANCE
+};
