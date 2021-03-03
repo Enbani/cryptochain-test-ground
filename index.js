a3d6fef399637ff81aac175205089e4b16669f70
Enbani <enbani.moore@gmail.com>
2021-03-02 20:50:37 -0500

    adding a network component, broadcast new blocks to the entire network

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const PubSub = require('./pubsub');
 const Blockchain = require('./blockchain');
 
 
 const app = express();
 const blockchain = new Blockchain();
-const PORT = 3001; //make this an env variable in future
+const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3001; //make this an env variable in future
+const ROOT_NODE_ADDRESS =  `http://localhost:${DEFAULT_PORT}`;
 
 app.use(bodyParser.json());
 
@@ -20,10 +25,39 @@ app.post('/api/mine', (req, res) => {
 
   blockchain.addBlock({ data });
 
+  pubsub.broadcastChain();
+
   res.redirect('/api/blocks');
 });
 
+const syncChains = async () => {
+
+  try {
+    let res = await axios.get(`${ROOT_NODE_ADDRESS}/api/blocks`);
+
+    if (res.status === 200) {
+      const rootChain = res.data.payload;
+      console.log('replace chain on a sync with', rootChain);
+      blockchain.replaceChain(rootChain);
+    }
+
+  } catch (err) {
+    console.log('There was an error: ', err);
+  }
+};
+
+
+
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, () => {
-  console.log('app server started on port: ', PORT);
+  console.log(`app server started on port: ${ PORT}`);
+  syncChains();
 });
