7eb16c08bb982034b2a3b1c9ad517b48d4ff9e67
Enbani <enbani.moore@gmail.com>
2021-03-02 22:24:26 -0500

    adding verification for wallet signatures

--- wallet/index.js
+++ wallet/index.js
@@ -1,9 +1,21 @@
 const { STARTING_BALANCE } = require('../config');
+const { ec } = require('../util');
+const cryptoHash = require('../util/crypto-hash');
+
+
 class Wallet {
   constructor() {
     this.balance = STARTING_BALANCE;
-    // this.publicKey = publicKey;
+    this.keyPair = ec.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode('hex');
   }
+
+  sign(data) {
+    const signature = this.keyPair.sign(cryptoHash(data))
+    return signature
+  }
+
+
 }
 
 module.exports = Wallet;
