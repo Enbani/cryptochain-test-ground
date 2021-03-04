cf5a081dc87b95593339ebd27459f8f6f27598ba
Enbani <enbani.moore@gmail.com>
2021-03-03 20:49:01 -0500

    adding txn inputs

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -3,7 +3,8 @@ const uuid = require('uuid');
 class Transaction {
   constructor({ senderWallet, recipient, amount }) {
     this.id = uuid.v4();
-    this.outputMap = this.createOutputMap({ senderWallet, recipient, amount })
+    this.outputMap = this.createOutputMap({ senderWallet, recipient, amount });
+    this.input = this.createInput({ senderWallet, outputMap: this.outputMap });
   }
 
   createOutputMap({ senderWallet, recipient, amount }) {
@@ -13,6 +14,17 @@ class Transaction {
     outputMap[senderWallet.publicKey] = senderWallet.balance - amount;
 
     return outputMap;
+  }
+
+  createInput({ senderWallet, outputMap }) {
+    let input = {
+      timestamp: Date.now(),
+      amount: senderWallet.balance,
+      address: senderWallet.publicKey,
+      signature: senderWallet.sign(outputMap)
+    };
+
+    return input;
   }
 }
 
